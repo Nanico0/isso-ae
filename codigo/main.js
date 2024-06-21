8c2aa2c35f3af0ae874c7be2379d6e928ecfe786
Nanico0 <168193542+Nanico0@users.noreply.github.com>
2024-06-21 09:52:29 -0300

    Add files via upload

---
+++ codigo/main.js
@@ -0,0 +1,30 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros ='0123456789';
+const simbolos = '!@%#*?';
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+const campoSEnha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhosenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20){
+        //tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanho
+}
